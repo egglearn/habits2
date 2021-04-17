0679910af8e6464d59a3f990a00ef63bf2ba7f09
marcus <marcushamlin1@gmail.com>
2021-04-17 15:37:51 +0200

    initial

---
+++ app.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCR5ZwXAjWfft2QgqJfbREISonxYsftTbs",
+  authDomain: "habits-4ad0c.firebaseapp.com",
+  projectId: "habits-4ad0c",
+  databaseURL:
+    "https://habits-4ad0c-default-rtdb.europe-west1.firebasedatabase.app/",
+  storageBucket: "habits-4ad0c.appspot.com",
+  messagingSenderId: "588630235509",
+  appId: "1:588630235509:web:213a6b2c7b47612662db3c",
+};
